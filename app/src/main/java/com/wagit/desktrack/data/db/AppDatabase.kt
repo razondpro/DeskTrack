2dc96b79bbbb822cc2a457a826077f1e93890db0
Razon Miah <miahrazon@gmail.com>
2021-06-14 13:26:42 +0200

    prepopulating data in db

--- app/src/main/java/com/wagit/desktrack/data/db/AppDatabase.kt
+++ app/src/main/java/com/wagit/desktrack/data/db/AppDatabase.kt
@@ -5,9 +5,15 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.wagit.desktrack.data.dao.RegistryDao
+import com.wagit.desktrack.data.dao.UserDao
 import com.wagit.desktrack.data.entities.Registry
 import com.wagit.desktrack.data.entities.User
 import com.wagit.desktrack.data.helpers.DateTimeTypeConverter
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import com.wagit.desktrack.data.helpers.PrepopulateData
 
 @Database(
     entities = [User::class, Registry::class],
@@ -18,21 +24,42 @@ import com.wagit.desktrack.data.helpers.DateTimeTypeConverter
 )
 abstract class AppDatabase : RoomDatabase() {
 
-    //database singleton
+    abstract fun userDao(): UserDao
+    abstract fun registryDao(): RegistryDao
+
     companion object {
         const val DB_VERSION = 1
         const val DB_NAME = "desktrack.db"
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        /**
+         * Get db instance
+         */
         fun getInstance(context: Context): AppDatabase{
             synchronized(this){
                 return INSTANCE?: Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     DB_NAME
-                ).build().also {
+                ).addCallback(dbCreateCallback(context))
+                 .build()
+                 .also {
                     INSTANCE = it
+                }
+            }
+        }
+
+        /**
+         * Prepopulating data in db
+         * Insert data on first run
+         */
+        private fun dbCreateCallback(context: Context) = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                GlobalScope.launch {
+                    getInstance(context).userDao()
+                        .insert(PrepopulateData.user)
                 }
             }
         }
---
+++ app/src/main/java/com/wagit/desktrack/data/helpers/PrepopulateData.kt
@@ -0,0 +1,7 @@
+package com.wagit.desktrack.data.helpers
+
+import com.wagit.desktrack.data.entities.User
+
+object PrepopulateData {
+    var user = User(0, "12345678z","John","Doe","admin123",true,false)
+}
