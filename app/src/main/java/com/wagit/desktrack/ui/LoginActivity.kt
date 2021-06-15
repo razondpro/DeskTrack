8819f98912c0fe5c15396b63119fe3877727776a
Razon Miah <miahrazon@gmail.com>
2021-06-15 18:49:01 +0200

    drawing login page

--- app/src/main/java/com/wagit/desktrack/ui/LoginActivity.kt
+++ app/src/main/java/com/wagit/desktrack/ui/LoginActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wagit.desktrack.R
 
-class DeskActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_desk)
+        setContentView(R.layout.activity_login)
     }
 }
