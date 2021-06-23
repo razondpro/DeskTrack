feb40fae83345e8e887f41057db67d6683ad4141
Razon Miah <miahrazon@gmail.com>
2021-06-23 13:03:39 +0200

    updating dependency injection class

--- app/src/main/java/com/wagit/desktrack/DI/DBModule.kt
+++ app/src/main/java/com/wagit/desktrack/DI/DBModule.kt
@@ -2,10 +2,14 @@ package com.wagit.desktrack.DI
 
 import android.content.Context
 import com.wagit.desktrack.data.dao.RegistryDao
-import com.wagit.desktrack.data.dao.UserDao
+import com.wagit.desktrack.data.dao.AccountDao
+import com.wagit.desktrack.data.dao.CompanyDao
+import com.wagit.desktrack.data.dao.EmployeeDao
 import com.wagit.desktrack.data.db.AppDatabase
 import com.wagit.desktrack.data.repositories.RegistryRepository
-import com.wagit.desktrack.data.repositories.UserRepository
+import com.wagit.desktrack.data.repositories.AccountRepository
+import com.wagit.desktrack.data.repositories.CompanyRepository
+import com.wagit.desktrack.data.repositories.EmployeeRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,10 +24,10 @@ object DBModule {
      * @return returns the Dao object to be used as dependency in Repository class.
      */
     @Provides
-    fun provideUserDao (
+    fun provideAccountDao (
         @ApplicationContext context: Context
-    ): UserDao {
-        return AppDatabase.getInstance(context).userDao()
+    ): AccountDao {
+        return AppDatabase.getInstance(context).accountDao()
     }
 
     /**
@@ -37,11 +41,29 @@ object DBModule {
     }
 
     /**
-     * Provides user repository
+     * @return returns the Dao object to be used as dependency in Repository class.
+     */
+    fun provideCompanyDao(
+        @ApplicationContext context: Context
+    ): CompanyDao {
+        return AppDatabase.getInstance(context).companyDao()
+    }
+
+    /**
+     * @return returns the Dao object to be used as dependency in Repository class.
+     */
+    fun provideEmployeeDao(
+        @ApplicationContext context: Context
+    ): EmployeeDao {
+        return AppDatabase.getInstance(context).employeeDao()
+    }
+
+    /**
+     * Provides account repository
      */
     @Provides
-    fun provideUserRepository(userDao: UserDao): UserRepository {
-        return UserRepository(userDao)
+    fun provideAccountRepository(accountDao: AccountDao): AccountRepository {
+        return AccountRepository(accountDao)
     }
 
     /**
@@ -50,6 +72,22 @@ object DBModule {
     @Provides
     fun provideRegistryRepository(registryDao: RegistryDao): RegistryRepository {
         return RegistryRepository(registryDao)
+    }
+
+    /**
+     * provides company repository
+     */
+    @Provides
+    fun provideCompanyRepository(companyDao: CompanyDao): CompanyRepository {
+        return CompanyRepository(companyDao)
+    }
+
+    /**
+     * Provies employee repository
+     */
+    @Provides
+    fun provideEmployeeRepository(employeeDao: EmployeeDao): EmployeeRepository {
+        return provideEmployeeRepository(employeeDao)
     }
 
 }
