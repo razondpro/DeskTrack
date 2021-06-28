be7702cef6b000a3515e6e9cb00194f152a6d7a4
Razon Miah <miahrazon@gmail.com>
2021-06-28 08:55:18 +0200

    saving project state

--- app/src/main/java/com/wagit/desktrack/ui/user/home/view/HomeFragment.kt
+++ app/src/main/java/com/wagit/desktrack/ui/user/home/view/HomeFragment.kt
@@ -1,21 +1,35 @@
 package com.wagit.desktrack.ui.user.home.view
 
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.wagit.desktrack.R
 import com.wagit.desktrack.databinding.FragmentHomeBinding
 import com.wagit.desktrack.ui.BaseFragment
+import com.wagit.desktrack.ui.user.home.viewmodel.HomeViewModel
 import com.wagit.desktrack.ui.user.viewmodel.SharedHomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    private val viewModel: SharedHomeViewModel by activityViewModels()
+    private val sharedViewModel: SharedHomeViewModel by activityViewModels()
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun FragmentHomeBinding.initialize() {
         println("HHOLA FROM HOME")
-        println(viewModel.employee.value)
-        this.sharedVM = viewModel
+        println(sharedViewModel.employee.value)
+        this.sharedVM = sharedViewModel
+        val treg = homeViewModel.getTodaysRegistry(sharedViewModel.employee.value!!.id)
+
+        homeViewModel.tRegistry.observe(viewLifecycleOwner, Observer {
+            println("--------")
+            println(it.first())
+            println("--------")
+        })
+
+
+
     }
 
 }
